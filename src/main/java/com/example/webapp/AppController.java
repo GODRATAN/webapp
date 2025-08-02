7754ae8170291dd15bb50830d7dc3aeb6335bbfc
GODRATAN <ratanpandith@gmail.com>
2025-08-02 10:32:29 +0530

    add output

--- src/main/java/com/example/webapp/AppController.java
+++ src/main/java/com/example/webapp/AppController.java
@@ -1,5 +1,14 @@
 package com.example.webapp;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@CrossOrigin
 public class AppController {
-    
+    @GetMapping("/add/{a}/{b}")
+    public String add(@PathVariable("a") int a, @PathVariable("b") int b) {
+        return "Addition of two numbers : " + (a + b);
+    }
 }
